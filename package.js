4023ab215c8446f65966b658a279a673e5d5e6e0
Netanel Gilad <netanelgilad@gmail.com>
2014-09-06 11:12:36 +0300

    initial commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  summary: " Mentions and Macros for Angular",
+  version: "0.0.1",
+  git: "https://github.com/netanelgilad/meteor-ment.io.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1');
+
+  api.use('urigo:ngmeteor@0.2.0', 'client');
+
+  api.addFiles('mentio.min.js');
+  api.addFiles('templates.js');
+
+  api.addFiles('init.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('netanelgilad:ment.io');
+  api.addFiles('netanelgilad:ment.io-tests.js');
+});
