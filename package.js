e7470df678ab7e6acb2d6a9847eb57f5ebf80566
Netanel Gilad <netanelgilad@gmail.com>
2014-09-07 09:40:03 +0300

    changed to full version...

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: " Mentions and Macros for Angular",
-  version: "0.0.3",
+  version: "0.0.4",
   git: "https://github.com/netanelgilad/meteor-ment.io.git"
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
 
   api.use('urigo:ngmeteor@0.2.0', 'client');
 
-  api.addFiles('mentio.min.js', 'client');
+  api.addFiles('mentio.js', 'client');
   api.addFiles('templates.js', 'client');
 
   api.addFiles('init.js', 'client');
