b331af8828b20ca3a8b57cc5ecec5d73ed8e4932
David Yahalomi <davidyaha@gmail.com>
2015-03-10 21:54:23 +0200

    Upgraded to angular-meteor 0.7.2

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   summary: " Mentions and Macros for Angular",
-  version: "0.0.6",
-  git: "https://github.com/netanelgilad/meteor-ment.io.git"
+  version: "0.0.7",
+  git: "https://github.com/netanelgilad/meteor-ment.io.git",
+  name: "netanelgilad:ment.io"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1');
 
-  api.use('urigo:angular@0.4.2', 'client');
+  api.use('urigo:angular@0.7.2', 'client');
 
-  api.addFiles('mentio.js', 'client');
-  api.addFiles('templates.js', 'client');
+  api.addFiles('mentio.min.js', 'client');
 
   api.addFiles('init.js', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('netanelgilad:ment.io');
-  api.addFiles('netanelgilad:ment.io-tests.js');
+  api.use('netanelgilad:ment.io', 'client');
+  api.addFiles('netanelgilad:ment.io-tests.js', 'client');
 });
